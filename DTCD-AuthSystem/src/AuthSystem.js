a8e35f87a6ec04db832e5e6e114b97e0e1f00da5
rkuramshin <rkuramshin@isgneuro.com>
2022-04-04 17:54:46 +0400

    init

---
+++ DTCD-AuthSystem/src/AuthSystem.js
@@ -0,0 +1,76 @@
+import { SystemPlugin, LogSystemAdapter, InteractionSystemAdapter } from './../../DTCD-SDK';
+import { version } from '../package.json';
+
+export class AuthSystem extends SystemPlugin {
+  #guid;
+  #logSystem;
+  #interactionSystem;
+  #isLogged = false;
+
+  /**
+   * @constructor
+   * @param {String} guid guid of system instance
+   */
+  constructor(guid) {
+    super();
+    this.#guid = guid;
+    this.#logSystem = new LogSystemAdapter('0.5.0', this.#guid, 'AuthSystem');
+    this.#interactionSystem = new InteractionSystemAdapter('0.4.0');
+  }
+
+  async init() {
+    const { data: response } = await this.#interactionSystem.GETRequest('/api/isLoggedIn');
+    if (response.isLoggedIn) {
+      this.#isLogged = true;
+      return;
+    }
+    this.#isLogged = false;
+  }
+
+  /**
+   * Returns meta information about plugin for registration in application
+   * @returns {Object} - meta-info
+   */
+  static getRegistrationMeta() {
+    return {
+      type: 'core',
+      title: 'Система аутентификации',
+      name: 'AuthSystem',
+      version,
+      withDependencies: false,
+      priority: 1.5,
+    };
+  }
+
+  /**
+   * Returns guid of AuthSystem instance
+   * @returns {String} - guid
+   */
+  get guid() {
+    return this.#guid;
+  }
+
+  get isLoggedIn() {
+    return this.#isLogged;
+  }
+
+  async authorize(login, password) {
+    let response = await this.#interactionSystem.POSTRequest('/api/login', {
+      login,
+      password,
+    });
+
+    if (response.status === 200) {
+      this.#isLogged = true;
+      return this.#isLogged;
+    }
+
+    this.#isLogged = false;
+    return this.#isLogged;
+  }
+
+  async logout() {
+    await this.#interactionSystem.POSTRequest('/api/logout', {});
+    this.#isLogged = false;
+  }
+}
